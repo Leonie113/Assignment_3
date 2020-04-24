1ada1007234591fa74c549bf10c67d3b7fda720c
Leonie113 <57359744+Leonie113@users.noreply.github.com>
2020-04-24 12:17:43 +0200

    finale Version textImg Leonie

--- assignment3/src/components.d.ts
+++ assignment3/src/components.d.ts
@@ -6,87 +6,50 @@
  */
 import { HTMLStencilElement, JSXBase } from "@stencil/core/internal";
 export namespace Components {
-    interface MyComponent {
-        /**
-          * The first name
-         */
-        "first": string;
-        /**
-          * The last name
-         */
-        "last": string;
-        /**
-          * The middle name
-         */
-        "middle": string;
-    }
     interface SearchLeonie {
     }
-    interface TextslideLeonie {
+    interface TextimgLeonie {
     }
 }
 declare global {
-    interface HTMLMyComponentElement extends Components.MyComponent, HTMLStencilElement {
-    }
-    var HTMLMyComponentElement: {
-        prototype: HTMLMyComponentElement;
-        new (): HTMLMyComponentElement;
-    };
     interface HTMLSearchLeonieElement extends Components.SearchLeonie, HTMLStencilElement {
     }
     var HTMLSearchLeonieElement: {
         prototype: HTMLSearchLeonieElement;
         new (): HTMLSearchLeonieElement;
     };
-    interface HTMLTextslideLeonieElement extends Components.TextslideLeonie, HTMLStencilElement {
+    interface HTMLTextimgLeonieElement extends Components.TextimgLeonie, HTMLStencilElement {
     }
-    var HTMLTextslideLeonieElement: {
-        prototype: HTMLTextslideLeonieElement;
-        new (): HTMLTextslideLeonieElement;
+    var HTMLTextimgLeonieElement: {
+        prototype: HTMLTextimgLeonieElement;
+        new (): HTMLTextimgLeonieElement;
     };
     interface HTMLElementTagNameMap {
-        "my-component": HTMLMyComponentElement;
         "search-leonie": HTMLSearchLeonieElement;
-        "textslide-leonie": HTMLTextslideLeonieElement;
+        "textimg-leonie": HTMLTextimgLeonieElement;
     }
 }
 declare namespace LocalJSX {
-    interface MyComponent {
-        /**
-          * The first name
-         */
-        "first"?: string;
-        /**
-          * The last name
-         */
-        "last"?: string;
-        /**
-          * The middle name
-         */
-        "middle"?: string;
-    }
     interface SearchLeonie {
         "onEvClick"?: (event: CustomEvent<any>) => void;
         "onEvClose"?: (event: CustomEvent<any>) => void;
         "onEvSearch"?: (event: CustomEvent<any>) => void;
     }
-    interface TextslideLeonie {
+    interface TextimgLeonie {
         "onEvHover"?: (event: CustomEvent<any>) => void;
         "onEvText"?: (event: CustomEvent<any>) => void;
     }
     interface IntrinsicElements {
-        "my-component": MyComponent;
         "search-leonie": SearchLeonie;
-        "textslide-leonie": TextslideLeonie;
+        "textimg-leonie": TextimgLeonie;
     }
 }
 export { LocalJSX as JSX };
 declare module "@stencil/core" {
     export namespace JSX {
         interface IntrinsicElements {
-            "my-component": LocalJSX.MyComponent & JSXBase.HTMLAttributes<HTMLMyComponentElement>;
             "search-leonie": LocalJSX.SearchLeonie & JSXBase.HTMLAttributes<HTMLSearchLeonieElement>;
-            "textslide-leonie": LocalJSX.TextslideLeonie & JSXBase.HTMLAttributes<HTMLTextslideLeonieElement>;
+            "textimg-leonie": LocalJSX.TextimgLeonie & JSXBase.HTMLAttributes<HTMLTextimgLeonieElement>;
         }
     }
 }
